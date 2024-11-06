1131547c43a06ed66dcb4f22ef82ec8bf49d0538
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2024-11-05 22:33:37 -0300

    Removing connection deprecated options

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const app = express();
 app.use(express.json());
 
 // Conexão com o MongoDB
-mongoose.connect('mongodb://localhost:27017/produtosDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Conectado ao MongoDB'))
+mongoose.connect('mongodb://localhost:27017/produtosDB', {})
+  .then(() => console.log('Conectado ao MongoDB'))
   .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
-
+ 
 // Rota inicial de teste
 app.get('/', (req, res) => {
   res.send('API de Produtos');
